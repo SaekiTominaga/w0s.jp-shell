140127b5106376867ed7206769395d6ba050fded
SaekiTominaga <kcsu3470+github@gmail.com>
2021-02-06 15:06:17 +0900

    クラスコメント書いた

--- src/component/Test.ts
+++ src/component/Test.ts
@@ -1,6 +1,9 @@
 import Component from '../Component.js';
 import ComponentInterface from '../ComponentInterface.js';
 
+/**
+ * シェル機能のテスト用
+ */
 export default class Test extends Component implements ComponentInterface {
 	/**
 	 * @param {string[]} args - Arguments passed to the script
