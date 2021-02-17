11d716e12d5ac451acaf2febdcf7c7483756cc01
SaekiTominaga <kcsu3470+github@gmail.com>
2021-02-18 00:09:35 +0900

    メディアアップロードに対応

--- src/util/Tweet.ts
+++ src/util/Tweet.ts
@@ -4,9 +4,10 @@ import TwitterText from 'twitter-text';
 export default class Tweet {
 	#twitter: Twitter;
 
-	/* APIのアクセス取得間隔（ミリ秒） */
+	/* 画像の添付最大数 */
+	readonly #IMAGE_LIMIT = 4;
+	/* API のアクセス取得間隔（ミリ秒） */
 	readonly #ACCESS_INTERVAL = 1000;
-
 	/* 最大文字数超過時の本文末尾に追加する文字列 */
 	readonly #POST_MARKER = '...';
 
@@ -18,24 +19,17 @@ export default class Tweet {
 	}
 
 	/**
-	 * API接続前に行う処理
-	 */
-	private async _apiConnectPreprocessing(): Promise<void> {
-		if (this.#requestCount > 0) {
-			/* 初回リクエスト時以外は一定間隔を空けてアクセス */
-			await new Promise((resolve) => setTimeout(resolve, this.#ACCESS_INTERVAL));
-		}
-		this.#requestCount++;
-	}
-
-	/**
 	 * 投稿する
 	 *
 	 * @param {string} text - 本文
 	 * @param {string} url - URL
 	 * @param {string} hashtag - ハッシュタグ
+	 * @param {string} medias - 添付するメディア
 	 */
-	async postMessage(text: string, url = '', hashtag = ''): Promise<void> {
+	async postMessage(text: string, url?: string, hashtag?: string, medias?: Buffer[]): Promise<Twitter.ResponseData> {
+		const requestParams: Twitter.RequestParams = {};
+
+		/* 本文を組み立てる */
 		let postText = text;
 		let postMessage = this._assembleTweetMessage(postText, url, hashtag);
 
@@ -48,15 +42,62 @@ export default class Tweet {
 			}
 		}
 
+		requestParams.status = postMessage;
+
+		/* メディアをアップロードする */
+		if (medias !== undefined) {
+			if (medias.length > this.#IMAGE_LIMIT) {
+				throw new RangeError(`There should be no more than ${this.#IMAGE_LIMIT} media attachments.`);
+			}
+
+			const mediaIds: Set<string> = new Set();
+			for (const media of medias) {
+				mediaIds.add(await this.uploadMedia(media));
+			}
+
+			requestParams.media_ids = Array.from(mediaIds).join(',');
+		}
+
 		await this._apiConnectPreprocessing();
 
-		const response = await this.#twitter.post('statuses/update', {
-			status: postMessage,
-		}); // https://developer.twitter.com/en/docs/twitter-api/v1/tweets/post-and-engage/api-reference/post-statuses-update
+		const response = await this.#twitter.post('statuses/update', requestParams); // https://developer.twitter.com/en/docs/twitter-api/v1/tweets/post-and-engage/api-reference/post-statuses-update
 
 		if (response.text === undefined) {
 			throw new Error(`Tweet failure. ${response.toString()}`);
 		}
+
+		return response;
+	}
+
+	/**
+	 * メディアをアップロードする
+	 *
+	 * @param {Buffer} media - アップロードするメディア
+	 * @returns {string} media_id
+	 */
+	async uploadMedia(media: Buffer): Promise<string> {
+		await this._apiConnectPreprocessing();
+
+		const response = await this.#twitter.post('media/upload', {
+			media: media,
+		}); // https://developer.twitter.com/en/docs/twitter-api/v1/media/upload-media/api-reference/post-media-upload
+
+		if (response.media_id_string === undefined) {
+			throw new Error(`Media upload failure. ${response.toString()}`);
+		}
+
+		return response.media_id_string;
+	}
+
+	/**
+	 * API 接続前に行う処理
+	 */
+	private async _apiConnectPreprocessing(): Promise<void> {
+		if (this.#requestCount > 0) {
+			/* 初回リクエスト時以外は一定間隔を空けてアクセス */
+			await new Promise((resolve) => setTimeout(resolve, this.#ACCESS_INTERVAL));
+		}
+		this.#requestCount++;
 	}
 
 	/**
@@ -69,15 +110,15 @@ export default class Tweet {
 	 *
 	 * @returns {string} 組み立てたメッセージ
 	 */
-	private _assembleTweetMessage(text: string, url = '', hashtag = '', trimMaker = ''): string {
+	private _assembleTweetMessage(text: string, url?: string, hashtag?: string, trimMaker?: string): string {
 		let message = text;
-		if (trimMaker !== undefined) {
+		if (trimMaker !== undefined && trimMaker !== '') {
 			message += trimMaker;
 		}
-		if (hashtag !== '') {
+		if (hashtag !== undefined && hashtag !== '') {
 			message += ` ${hashtag}`;
 		}
-		if (url !== '') {
+		if (url !== undefined && url !== '') {
 			message += `\n${url}`;
 		}
 
