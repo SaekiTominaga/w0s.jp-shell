fe5c28f154ef22c638284037236350032b73cfe7
SaekiTominaga <kcsu3470+github@gmail.com>
2022-05-23 21:50:16 +0900

    #16 ログイン処理を直接 POST から、いったんトップページにアクセスしてログインボタンを押下するように変更（問題なければこのままリリースする）

--- node/configure/type/yokohama-library-hold-notice.d.ts
+++ node/configure/type/yokohama-library-hold-notice.d.ts
@@ -9,29 +9,40 @@
  * 自然言語による、人間が見て分かりやすい名前を設定する。通知メールの件名などで使用される。
  */
 export type NoName1 = string;
+export type ID = string;
+export type NoName2 = string;
 export type URL = string;
-export type URLPOST = string;
-export type NoName3 = string;
+export type ID1 = string;
 export type NoName4 = string;
 export type NoName5 = string;
-export type NoName7 = string;
+export type NoName6 = string;
 export type NoName8 = string;
 export type NoName9 = string;
+export type NoName10 = string;
+export type NoName12 = string;
+export type NoName13 = string;
 
 export interface NoName {
   title: NoName1;
+  id: ID;
+  password: NoName2;
   url: URL;
-  urlParam: URLPOST;
-  ready: NoName2;
-  notice: NoName6;
+  login: NoName3;
+  ready: NoName7;
+  notice: NoName11;
 }
-export interface NoName2 {
-  wrapSelector: NoName3;
-  titleSelector: NoName4;
-  confirmButtonSelector: NoName5;
+export interface NoName3 {
+  idSelector: ID1;
+  passwordSelector: NoName4;
+  submitSelector: NoName5;
+  errorSelector: NoName6;
 }
-export interface NoName6 {
-  messagePrefix: NoName7;
-  messageSuffix: NoName8;
-  separator: NoName9;
+export interface NoName7 {
+  wrapSelector: NoName8;
+  titleSelector: NoName9;
+  confirmButtonSelector: NoName10;
+}
+export interface NoName11 {
+  messagePrefix: NoName12;
+  messageSuffix: NoName13;
 }
--- node/src/component/YokohamaLibraryHoldNotice.ts
+++ node/src/component/YokohamaLibraryHoldNotice.ts
@@ -24,51 +24,59 @@ export default class YokohamaLibraryHoldNotice extends Component implements Comp
 			const page = await browser.newPage();
 			await page.setRequestInterception(true);
 			page.on('request', (request) => {
-				request.continue({
-					method: 'POST',
-					headers: {
-						'Content-Type': 'application/x-www-form-urlencoded',
-					},
-					postData: this.#config.urlParam,
-				});
+				request.continue();
 			});
-			const response = await page.goto(this.#config.url, { waitUntil: 'domcontentloaded' });
 
-			/* レスポンスボディ */
-			const responseBody = await response.text();
-			this.logger.debug(responseBody);
+			/* ログイン */
+			await page.goto(this.#config.url, { waitUntil: 'domcontentloaded' });
+			await page.type(this.#config.login.idSelector, this.#config.id);
+			await page.type(this.#config.login.passwordSelector, this.#config.password);
+			await Promise.all([page.click(this.#config.login.submitSelector), page.waitForNavigation()]);
+			this.logger.debug(`ログインボタン（${this.#config.login.submitSelector}）押下`);
+
+			this.logger.info('ログイン後ページ', page.url());
+
+			const response = await page.content();
+			this.logger.debug(response);
 
 			/* DOM 化 */
-			const document = new JSDOM(responseBody).window.document;
+			const document = new JSDOM(response).window.document;
 
-			const readyBookWrapElements = document.querySelectorAll(this.#config.ready.wrapSelector);
-			if (readyBookWrapElements.length === 0) {
-				this.logger.info('新着予約なし');
+			const errorElement = document.querySelector(this.#config.login.errorSelector);
+			if (errorElement !== null) {
+				/* エラーメッセージがある場合 */
+				this.logger.warn(errorElement.textContent?.trim());
 			} else {
-				const readyBookTitleList: string[] = [];
+				const readyBookWrapElements = document.querySelectorAll(this.#config.ready.wrapSelector);
+				if (readyBookWrapElements.length === 0) {
+					this.logger.info('新着予約なし');
+				} else {
+					const readyBookTitleList: string[] = [];
 
-				for (const readyWrapElement of readyBookWrapElements) {
-					const readyBookTitleElement = readyWrapElement.querySelector(this.#config.ready.titleSelector);
-					if (readyBookTitleElement === null) {
-						throw new Error(`書名の HTML 要素（${this.#config.ready.titleSelector}）が存在しない。`);
+					for (const readyWrapElement of readyBookWrapElements) {
+						const readyBookTitleElement = readyWrapElement.querySelector(this.#config.ready.titleSelector);
+						if (readyBookTitleElement === null) {
+							throw new Error(`書名の HTML 要素（${this.#config.ready.titleSelector}）が存在しない。`);
+						}
+
+						const readyBookTitle = readyBookTitleElement.textContent;
+						if (readyBookTitle === null) {
+							throw new Error(`書名の HTML 要素（${this.#config.ready.titleSelector}）の内容が空。`);
+						}
+
+						readyBookTitleList.push(readyBookTitle.replace(/^　+|　+$/, '')); // eslint-disable-line no-irregular-whitespace
 					}
 
-					const readyBookTitle = readyBookTitleElement.textContent;
-					if (readyBookTitle === null) {
-						throw new Error(`書名の HTML 要素（${this.#config.ready.titleSelector}）の内容が空。`);
+					this.notice.push(`${this.#config.notice.messagePrefix}${readyBookTitleList.join('\n')}\n\n${this.#config.url}${this.#config.notice.messageSuffix}`);
+
+					const confirmButton = await page.$(this.#config.ready.confirmButtonSelector);
+					if (confirmButton === null) {
+						throw new Error(`確認ボタン（${this.#config.ready.confirmButtonSelector}）が存在しない。`);
 					}
 
-					readyBookTitleList.push(readyBookTitle.replace(/^　+|　+$/, '')); // eslint-disable-line no-irregular-whitespace
+					await Promise.all([confirmButton.click(), page.waitForNavigation()]);
+					this.logger.debug(`確認ボタン（${this.#config.ready.confirmButtonSelector}）押下`);
 				}
-
-				this.notice.push(`${this.#config.notice.messagePrefix}${readyBookTitleList.join(this.#config.notice.separator)}${this.#config.notice.messageSuffix}`);
-
-				const confirmButton = await page.$(this.#config.ready.confirmButtonSelector);
-				if (confirmButton === null) {
-					throw new Error(`確認ボタン（${this.#config.ready.confirmButtonSelector}）が存在しない。`);
-				}
-				this.logger.debug(`確認ボタン（${this.#config.ready.confirmButtonSelector}）押下`);
-				await Promise.all([page.waitForNavigation(), confirmButton.click()]);
 			}
 		} finally {
 			this.logger.debug('browser.close()');
