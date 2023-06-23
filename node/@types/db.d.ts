2f63d964b3c5cddea25269a3dffd353cac52a70a
SaekiTominaga <kcsu3470+github@gmail.com>
2023-06-23 11:41:45 +0900

    Amazon 関連の機能はもう使っていないので削除

--- node/@types/db.d.ts
+++ node/@types/db.d.ts
@@ -1,31 +1,3 @@
-declare namespace AmazonAdsDb {
-	export interface Dp {
-		asin: string;
-		dp_url: string;
-		title: string;
-		binding: string | null;
-		publication_date: Date | null;
-		image_url: string | null;
-		image_width: number | null;
-		image_height: number | null;
-	}
-}
-
-declare namespace BlogDb {
-	export interface AmazonDp {
-		asin: string;
-		dp_url: string;
-		title: string;
-		binding: string | null;
-		product_group: string | null;
-		publication_date: Date | null;
-		image_url: string | null;
-		image_width: number | null;
-		image_height: number | null;
-		modified_at: Date | null;
-	}
-}
-
 declare namespace CrawlerDb {
 	export interface News {
 		url: string;
