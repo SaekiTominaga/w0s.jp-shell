76528c5e3a1301db2585b7a8fc48c5da18ca1ec6
SaekiTominaga <kcsu3470+github@gmail.com>
2026-01-22 21:20:37 +0900

    ブラウザアクセスから fetch() へ変更

--- node/src/config/jrCyberStation.ts
+++ node/src/config/jrCyberStation.ts
@@ -1,4 +1,5 @@
 export default {
+	topUrl: 'https://www.jr.cyberstation.ne.jp/',
 	stationUrl: 'https://www.jr.cyberstation.ne.jp/js/ja/station.js',
 	searchUrl: 'https://www.jr.cyberstation.ne.jp/jcs/Vacancy.do',
 	searchInterval: 1,
