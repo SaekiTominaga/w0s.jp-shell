80975e86b48e21108c6cfadeefcde6bc38872e0f
SaekiTominaga <kcsu3470+github@gmail.com>
2026-07-09 17:08:25 +0900

    各コンポーネントと共通で分離していた通知機能を統合する

--- node/src/shell.ts
+++ node/src/shell.ts
@@ -1,6 +1,5 @@
 import { parseArgs } from 'node:util';
 import type { Logger } from 'winston';
-import { env } from '@w0s/env-value-type';
 import { getLogger } from './logger.ts';
 import Notice from './Notice.ts';
 import ProcessTime from './util/ProcessTime.ts';
@@ -42,24 +41,22 @@ const logger = getLogger(componentName);
 
 logger.info('----- Start processing');
 
-const notice = new Notice(env('NOTICE_MAIL_TITLE'));
-const componentNotice = new Notice(noticeTitle);
+const notice = new Notice(noticeTitle);
 
 try {
 	/* コンポーネントの読み込みと実行 */
 	// eslint-disable-next-line @typescript-eslint/no-unsafe-call, @typescript-eslint/no-unsafe-member-access
-	await (await import(`./component/${componentName}.ts`)).default({ logger, notice: componentNotice });
+	await (await import(`./component/${componentName}.ts`)).default({ logger, notice });
 } catch (e) {
 	logger.error(e);
 } finally {
-	/* 通知送信 */
-	await componentNotice.send();
-
 	/* タイムアウト判定 */
 	const processingTime = processTime.getTime();
 	if (timeout > 0 && processingTime > timeout) {
 		notice.add(`\`${componentName}\` の実行時間過多: ${ProcessTime.format(processingTime)}`);
 	}
+
+	/* 通知送信 */
 	await notice.send();
 
 	logger.info(`End of process: ${String(processingTime)}s -----`);
